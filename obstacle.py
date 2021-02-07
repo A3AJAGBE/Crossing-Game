12bb1e4c3677f0c08f9b5a66aac587070257a24c
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-07 07:55:59 +0000

    Refactor

--- obstacle.py
+++ obstacle.py
@@ -1,23 +1,30 @@
 from turtle import Turtle
+import random
+
 COLORS = ["medium spring green", "light sea green", "pink", "pale violet red", "medium purple", "light salmon",
           "pale green", "pale turquoise"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class Obstacle():
+class Obstacle:
 
     def __init__(self):
         self.obstacles = []
-        self.posY = -220
-        self.generate()
 
     def generate(self):
-        for color in COLORS:
+        # To prevent the obstacle from cluster
+        num = random.randint(1, 4)
+        if num == 1:
             obstacle = Turtle("circle")
+            obstacle.shapesize(stretch_wid=1, stretch_len=2)
             obstacle.penup()
-            obstacle.color(color)
-            obstacle.goto(380, self.posY)
-            self.posY += 60
+            obstacle.color(random.choice(COLORS))
+            posY = random.randint(-220, 220)
+            obstacle.goto(400, posY)
             self.obstacles.append(obstacle)
+
+    def move_obstacle(self):
+        for ball in self.obstacles:
+            ball.backward(STARTING_MOVE_DISTANCE)
 
