bfeeb7d9d66e5f2304137ce0e6a1b8f21b382b5d
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-07 05:41:54 +0000

    Update with player function

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from turtle import Screen
+from player import Player
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("ivory")
 screen.title("A3AJAGBE Crossing Game")
+screen.tracer(0)
+
+player = Player()
+
+# Moving the turtle
+screen.listen()
+screen.onkey(player.move, "Up")
+
+# The game application
+start = True
+while start:
+    time.sleep(0.1)
+    screen.update()
+
 
 # Close only when exited
 screen.exitonclick()
