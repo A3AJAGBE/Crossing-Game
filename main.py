cc7e45f31954ed80662670bb512a53c9e9948677
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-07 05:19:11 +0000

    Screen set up

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("ivory")
+screen.title("A3AJAGBE Crossing Game")
+
+# Close only when exited
+screen.exitonclick()
